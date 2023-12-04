00b784672749692f27a6ef5c98c827d55c55ee72
Bartłomiej Jacak <bartek.jacak@gmail.com>
2023-12-04 23:55:11 +0100

    Add template for future days

---
+++ template/index.js
@@ -0,0 +1,14 @@
+const inputText = await Bun.file("./input.txt").text();
+const inputArray = inputText.split("\n");
+
+// Consts
+
+function runPartOne(input) {}
+
+function runPartTwo(input) {}
+
+// Results
+console.log("Part 1: ", runPartOne(inputArray));
+console.log("Part 2: ", runPartTwo(inputArray));
+
+// Utils
